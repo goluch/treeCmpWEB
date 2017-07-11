2d7860a1d9584237b29c88ed4665136be1131ab4
Tomasz Goluch <tomasz.goluch@gmail.com>
2017-07-11 12:05:43 +0200

    Changed static resources location

--- src/main/java/pl/gda/pg/eti/WebConfig.java
+++ src/main/java/pl/gda/pg/eti/WebConfig.java
@@ -20,9 +20,20 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         configurer.enable();
     }
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
 
 }
